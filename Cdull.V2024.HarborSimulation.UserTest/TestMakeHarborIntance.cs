ded1162508a0446b802963305362cef2d1d91b53
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2024-02-14 11:07:30 +0100

    Still working on UserTests

---
+++ Cdull.V2024.HarborSimulation.UserTest/TestMakeHarborIntance.cs
@@ -0,0 +1,25 @@
+﻿using Cdull.V2024.HarborSimulation.SimulationFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cdull.V2024.HarborSimulation.UserTest
+{
+    public class TestMakeHarborIntance
+    {
+        static void Main(string[] args)
+        {
+
+            // Lag en Habor instans ved hjelp av konstruktøren: 
+
+            
+
+        }
+
+
+
+
+    }
+}
