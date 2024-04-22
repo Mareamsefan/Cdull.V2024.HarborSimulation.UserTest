f8d20d54914c0d63112689c4baa4b0106305518f
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2024-04-22 14:40:46 +0200

    updated test.

--- Cdull.V2024.HarborSimulation.UserTest/TestAll.cs
+++ Cdull.V2024.HarborSimulation.UserTest/TestAll.cs
@@ -1,40 +1,78 @@
 ﻿using Cdull.V2024.HarborSimulation.SimulationFramework;
-using Cdull.V2024.HarborSimulation.TestClient;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Cdull.V2024.HarborSimulation.SimulationFramework.Infrastructure;
+using Cdull.V2024.HarborSimulation.SimulationFramework.ShipOperations;
 
 namespace Cdull.V2024.HarborSimulation.UserTest
 {
     class TestAll
     {
         static void Main(string[] args)
-        {   
-            // Opprett en havn: 
+        {
+            // Welcome to the Harbor Simulation Usertest!
 
-            // Prøv å lag en liste med skip så enkelt så mulig ved å bruke harbor klassen:
-
-
-            // Prøv å lag en liste med kaiplasser så enkelt så mulig ved å bruke harbor klassen:
-
-
-            // Lag en simuleringen:  
-         
-            // Lag en starttid og en slutttid for simuleringen: 
-
-            
-            // Lag en starttid for når seilingen til alle skipene skal starte: 
+            // Follow these steps to set up and run the simulation:
            
 
-            // Prøv å kjør en simulering: 
-
+            // 1.Create a container storage.
             
-            // Prøv å hente ut historikken for simuleringen du nettopp kjørte:
+              // - Specify the name and location range for storage columns.
+            
 
 
-           
+           // 2.Set up the harbor.
+            
+            //  - Create a harbor with a name and location index range.
+            
+            //   - Initialize docks with cranes.
+            
+            //   - Create AGVs for container movement.
+
+
+            // 3.Add ships to the harbor.
+
+
+              // - Specify ship types, sizes, and locations.
+
+
+              // - Include any necessary cargo details.
+
+
+
+           // 4.Set up the simulation parameters.
+            
+              // - Define the start and end times for the simulation.
+
+
+           // 5.Schedule sailing for ships.
+
+
+              // - Specify sailing start dates, ship counts, and recurrence.
+
+
+
+           // 6.Prepare storage columns.
+            
+              // - Determine the locations and capacities of storage columns.
+            
+
+           // 7.Schedule container handling operations.
+            
+              // - Specify handling dates, ship assignments, and operations(loading / unloading).
+
+
+            //  8.Run the simulation.
+            
+             //  - Execute the simulation with the defined parameters.
+            
+
+          // 9.Check the simulation results.
+            
+              // - Review harbor and ship histories for specific dates.
+
+
+          // 10.Enjoy exploring the simulation outcome!
+
+
             
 
 
