08bc8789d49d8e766c18ddc1e548fd905c4155cd
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2024-04-24 12:53:58 +0200

    ready

--- Cdull.V2024.HarborSimulation.UserTest/TestAll.cs
+++ Cdull.V2024.HarborSimulation.UserTest/TestAll.cs
@@ -8,72 +8,9 @@ namespace Cdull.V2024.HarborSimulation.UserTest
     {
         static void Main(string[] args)
         {
-            // Welcome to the Harbor Simulation Usertest!
-
-            // Follow these steps to set up and run the simulation:
-           
-
-            // 1.Create a container storage.
-            
-              // - Specify the name and location range for storage columns.
             
 
 
-           // 2.Set up the harbor.
-            
-            //  - Create a harbor with a name and location index range.
-            
-            //   - Initialize docks with cranes.
-            
-            //   - Create AGVs for container movement.
-
-
-            // 3.Add ships to the harbor.
-
-
-              // - Specify ship types, sizes, and locations.
-
-
-              // - Include any necessary cargo details.
-
-
-
-           // 4.Set up the simulation parameters.
-            
-              // - Define the start and end times for the simulation.
-
-
-           // 5.Schedule sailing for ships.
-
-
-              // - Specify sailing start dates, ship counts, and recurrence.
-
-
-
-           // 6.Prepare storage columns.
-            
-              // - Determine the locations and capacities of storage columns.
-            
-
-           // 7.Schedule container handling operations.
-            
-              // - Specify handling dates, ship assignments, and operations(loading / unloading).
-
-
-            //  8.Run the simulation.
-            
-             //  - Execute the simulation with the defined parameters.
-            
-
-          // 9.Check the simulation results.
-            
-              // - Review harbor and ship histories for specific dates.
-
-
-          // 10.Enjoy exploring the simulation outcome!
-
-
-            
 
 
         }
